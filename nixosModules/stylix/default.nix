e053e67ab29436c1c0d8c62104129a6154fe0e44
Diego A. Estrada Rivera <daestrada@pm.me>
2025-02-25 08:54:07 -0400

    feat: Use moonfly for stylix.

--- nixosModules/stylix/default.nix
+++ nixosModules/stylix/default.nix
@@ -6,7 +6,27 @@
         stylix = {
             enable = true;
             autoEnable = true;
-            base16Scheme = "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
+            base16Scheme = {
+                slug = "moonfly";
+                scheme = "Theme by bluz71";
+                author = "bluz71";
+                base00 = "#080808";
+                base01 = "#323437";
+                base02 = "#949494";
+                base03 = "#9e9e9e";
+                base04 = "#bdbdbd";
+                base05 = "#c6c6c6";
+                base06 = "#e4e4e4";
+                base07 = "#eeeeee";
+                base08 = "#ff5454";
+                base09 = "#8cc85f";
+                base0A = "#e3c78a";
+                base0B = "#80a0ff";
+                base0C = "#cf87e8";
+                base0D = "#79dac8";
+                base0E = "#c6c6c6";
+                base0F = "#949494";
+            }; # "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
 
             image = ./sakura.png; # https://unsplash.com/photos/white-flowering-plant-1vuXf7ta_PU
 
