0224492c7e13bf7a4892d88747734185e20b885d
Sona Tau Estrada Rivera <sona@stau.space>
2025-07-31 00:47:06 -0400

    feat: New layout works!!!

--- hosts/fw13/home.nix
+++ hosts/fw13/home.nix
@@ -2,9 +2,12 @@
     imports = [
         ../../homeManagerModules
     ];
-    home.username = "diego";
-    home.homeDirectory = "/home/diego";
-    home.stateVersion = "23.11"; # WARNING: DO NOT EVER CHANGE THIS VALUE EVER !
+    home = {
+		username = "diego";
+		homeDirectory = "/home/diego";
+		stateVersion = "23.11"; # WARNING: DO NOT EVER CHANGE THIS VALUE EVER !
+		file."/home/diego/.xkb/symbols/mtgap-mod".source = ./mtgap-mod.xkb;
+	};
     rice.nier.enable = true;
     programs.home-manager.enable = true;
 }
