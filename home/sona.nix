1a5600c5cc8ea0c6b6c8edec2cd01f2b99a459ae
Sona Tau Estrada Rivera <sona@stau.space>
2026-02-04 21:38:55 -0400

    feat(sona.nix): Update sona user

--- home/sona.nix
+++ home/sona.nix
@@ -1,11 +1,44 @@
 { config, outputs, pkgs, ... }: {
 	imports = [
-		../homeManagerModules
+		../modules/home
 		# ../module/home/my.nix
 	];
 
 	rice.nier.enable = true;
 	programs.home-manager.enable = true;
+
+	my = {
+		shell = "zsh";
+		roles = {
+			wayland.pkgSet.enable = true;
+			terminal.pkgSet.enable = true;
+			browsers.pkgSet.enable = true;
+			webdev.pkgSet.enable = true;
+			lean.pkgSet.enable = true;
+			fun.pkgSet.enable = true;
+			writing.pkgSet.enable = true;
+
+			zen = {
+				enable = true;
+				pkgSet.enable = true;
+			};
+
+			base = {
+				enable = true;
+				pkgSet.enable = true;
+			};
+		};
+
+		stylix = {
+			enable = true;
+			theme = "zenbones";
+		};
+		starship.enable = true;
+		atuin.enable = true;
+		direnv.enable = true;
+		tealdeer.enable = true;
+		tmux.enable = true;
+	};
 
 	home = {
 		username = "sona";
