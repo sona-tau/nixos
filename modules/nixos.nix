77eb6cae4f4950dd8559b08e08618a83ec957f2b
Sona Tau Estrada Rivera <sona@stau.space>
2026-03-31 22:11:47 -0400

    feat: update nixos module

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -1,3 +1,27 @@
-{ ... }: {
-	flake.modules.nixos.base.imports = [ ../nixosModules/default.nix ];
+{ /* pkgs, */ ... }: {
+	flake.modules.nixos.base = {
+		nix.settings.experimental-features = [
+			"nix-command"
+			"flakes"
+		];
+
+		programs = {
+			nix-ld.enable = true;
+			steam.enable = true;
+			appimage = {
+				enable = true;
+				binfmt = true;
+				/*
+				package = pkgs.appimage-run.override {
+					extraPackages = pkgs: [
+						pkgs.icu
+						pkgs.libxcrypt-legacy
+						pkgs.python312
+						pkgs.python312Packages.torch
+					];
+				};
+				*/
+			};
+		};
+	};
 }
--- nixosModules/home/roles/base.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }: let cfg = config.my.roles.base; in {
-	options = {
-		my.roles.base.enable = lib.mkEnableOption "base";
-	};
-
-	config = lib.mkIf cfg.enable {
-		my.zsh.enable = true;
-	};
-}
