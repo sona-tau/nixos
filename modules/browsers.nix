cb3fb01eaedbea8db367abc13df75f7997d84587
Sona Tau Estrada Rivera <sona@stau.space>
2026-04-02 18:36:26 -0400

    refactor: simplify browsers module to use zen-browser specialArg

--- modules/browsers.nix
+++ modules/browsers.nix
@@ -1,36 +1,10 @@
 { ... }: {
-	flake.modules.homeManager.browsers = { config, lib, pkgs, inputs, ... }: let cfg = config.my.roles.browsers; in {
-		options = {
-			my.roles.browsers.enable = lib.mkEnableOption "default browsers (zen, librewolf, qutebrowsers)";
-		};
-
-		config = lib.mkIf cfg.enable {
-			home.packages = [
-				pkgs.librewolf
-				pkgs.qutebrowser
-				pkgs.firefoxpwa
-				(inputs.zen-browser.packages."x86_64-linux".default.overrideAttrs (final: prev: {
-					policies = { # find more options here: https://mozilla.github.io/policy-templates/
-						AutofillAddressEnabled = true;
-						AutofillCreditCardEnabled = false;
-						DisableAppUpdate = true;
-						DisableFeedbackCommands = true;
-						DisableFirefoxStudies = true;
-						DisablePocket = true;
-						DisableTelemetry = true;
-						DontCheckDefaultBrowser = true;
-						NoDefaultBookmarks = true;
-						OfferToSaveLogins = false;
-						EnableTrackingProtection = {
-							Value = true;
-							Locked = true;
-							Cryptomining = true;
-							Fingerprinting = true;
-						};
-					};
-					nativeMessagingHosts.packages = [ pkgs.firefoxpwa ];
-				 }))
-			];
-		};
+	flake.modules.homeManager.browsers = { pkgs, zen-browser, ... }: {
+		config.home.packages = [
+			pkgs.librewolf
+			pkgs.qutebrowser
+			pkgs.firefoxpwa
+			zen-browser
+		];
 	};
 }
