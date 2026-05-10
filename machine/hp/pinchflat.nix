5e0013453ca81b5dd7bf0782452103c06d969def
Sona Tau Estrada Rivera <sona@stau.space>
2026-05-10 12:05:06 -0400

    fix(hp): add media group so navidrome can read pinchflat's directory
    Both navidrome and pinchflat join the media group. /storage/storage/YouTube
    is declared 0750 pinchflat:media via systemd-tmpfiles.

--- machine/hp/pinchflat.nix
+++ machine/hp/pinchflat.nix
@@ -1,4 +1,15 @@
 { ... }: {
+	users.groups.media = {};
+
+	users.users = {
+		pinchflat.extraGroups = [ "media" ];
+		navidrome.extraGroups = [ "media" ];
+	};
+
+	systemd.tmpfiles.rules = [
+		"d /storage/storage/YouTube 0750 pinchflat media - -"
+	];
+
 	services.pinchflat = {
 		enable = true;
 		port = 8945;
