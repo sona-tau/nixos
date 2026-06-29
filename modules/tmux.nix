10f667eeb49dfb1665c89a5c296955c8da2a8a63
Sona Tau Estrada Rivera <sona@stau.space>
2026-06-28 21:17:53 -0400

    refactor(tmux): fix indentation, configure resurrect/continuum, switch terminal
    - fix top-level indentation (was double-indented under programs.tmux)
    - terminal: tmux-256color -> tmux-direct
    - windows/panes numbered from 1, renumber-windows on
    - fix copy-mode scroll-down binding (was scroll-up twice)
    - resurrect: explicit data dir, pane contents, nvim session strategy
    - continuum: auto-restore on, 15-min save interval

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,48 +1,68 @@
 { ... }: {
 	flake.modules.homeManager.tmux = { pkgs, ... }: {
-			programs.fzf.tmux.enableShellIntegration = true;
+		programs.fzf.tmux.enableShellIntegration = true;
 
-			programs.tmux = {
-				enable = true;
-				aggressiveResize = true;
-				customPaneNavigationAndResize = true;
-				clock24 = true;
-				escapeTime = 0;
-				prefix = "C-p";
-				sensibleOnTop = true;
-				shortcut = "a";
-				terminal = "tmux-256color";
-				historyLimit = 10000;
-				keyMode = "vi";
-				newSession = true;
+		programs.tmux = {
+			enable = true;
+			aggressiveResize = true;
+			customPaneNavigationAndResize = true;
+			clock24 = true;
+			escapeTime = 0;
+			prefix = "C-p";
+			sensibleOnTop = true;
+			shortcut = "a";
+			terminal = "tmux-direct";
+			historyLimit = 10000;
+			keyMode = "vi";
+			newSession = true;
 
-				extraConfig =''
-					tmux_conf_24b_colour=true
-					set -g xterm-keys on
-					set -s focus-events on
+			extraConfig = ''
+				tmux_conf_24b_colour=true
+				set -g xterm-keys on
+				set -s focus-events on
 
-					bind -n C-h copy-mode
-					bind -T copy-mode-vi u send-keys -X scroll-up
-					bind -T copy-mode-vi d send-keys -X scroll-up
+				# Windows and panes numbered from 1 (more intuitive)
+				set -g base-index 1
+				set -g pane-base-index 1
+				set-window-option -g pane-base-index 1
+				set-option -g renumber-windows on
 
-					bind r split-window -h
-					bind d split-window -v
+				bind -n C-h copy-mode
+				bind -T copy-mode-vi u send-keys -X scroll-up
+				bind -T copy-mode-vi d send-keys -X scroll-down
 
-					bind -n M-Left  select-pane -L
-					bind -n M-Right select-pane -R
-					bind -n M-Up	select-pane -U
-					bind -n M-Down  select-pane -D
+				bind r split-window -h
+				bind d split-window -v
 
-					set -as terminal-overrides ",foot*:Tc"
+				bind -n M-Left  select-pane -L
+				bind -n M-Right select-pane -R
+				bind -n M-Up    select-pane -U
+				bind -n M-Down  select-pane -D
+
+				set -as terminal-overrides ",foot*:Tc"
+			'';
+
+			plugins = with pkgs.tmuxPlugins; [
+				sensible
+				harpoon
+				better-mouse-mode
+				{
+					plugin = resurrect;
+					extraConfig = ''
+						set -g @resurrect-dir '~/.local/share/tmux/resurrect'
+						set -g @resurrect-capture-pane-contents 'on'
+						set -g @resurrect-strategy-nvim 'session'
+						set -g @resurrect-processes 'taskwarrior-tui khal aerc w3m'
 					'';
-
-				plugins = with pkgs.tmuxPlugins; [
-					sensible
-					resurrect
-					continuum
-					harpoon
-					better-mouse-mode
-				];
-			};
+				}
+				{
+					plugin = continuum;
+					extraConfig = ''
+						set -g @continuum-restore 'on'
+						set -g @continuum-save-interval '15'
+					'';
+				}
+			];
+		};
 	};
 }
