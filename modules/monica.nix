17f967fefb2860a410d0a84b4892cc0277f8f338
Sona Tau Estrada Rivera <sona@stau.space>
2026-06-29 11:22:27 -0400

    fix(monica): add phpfpm pm settings to pool config

--- modules/monica.nix
+++ modules/monica.nix
@@ -16,6 +16,11 @@
 				"listen.owner" = "caddy";
 				"listen.group" = "caddy";
 				"listen.mode"  = "0660";
+				"pm"                   = "dynamic";
+				"pm.max_children"      = 5;
+				"pm.start_servers"     = 2;
+				"pm.min_spare_servers" = 1;
+				"pm.max_spare_servers" = 3;
 			};
 		};
 
