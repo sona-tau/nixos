98f82dd197aade234af57f158b93cd016c0f89c2
Sona Tau Estrada Rivera <sona@stau.space>
2026-07-02 14:45:11 -0400

    feat: update flake nixpkgs-stable and flake inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+		nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
 		zen-browser.url = "github:0xc000022070/zen-browser-flake";
 		stylix.url = "github:danth/stylix";
 		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
